1bc640dc8f9c3ee1cc28052282706194323a72bc
poor314 <55570384+poor314@users.noreply.github.com>
2019-11-07 13:35:44 +0800

    Create heap_sort_06170213.py

---
+++ HW2/heap_sort_06170213.py
@@ -0,0 +1,29 @@
+def heapify(A,size,i):
+    l=2*i+1 
+    r=2*i+2 
+    largest=i 
+    
+    if l<size and A[l]>A[largest]:
+        largest=l
+        
+    if r<size and A[r]>A[largest]:
+        largest=r
+        
+    if largest!=i:
+        A[i],A[largest]=A[largest],A[i] 
+        
+        heapify(A,size,largest)
+def heapsort(A):
+    size=len(A)
+    
+    for i in range(size,-1,-1):
+        heapify(A,size,i)
+    
+    for i in range(size-1,0,-1):
+        A[i],A[0]=A[0],A[i]
+        
+        heapify(A,i,0)
+        
+A=[12,11,13,5,6,7]
+heapsort(A)
+print(A)
