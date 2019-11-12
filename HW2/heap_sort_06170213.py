556cb93dbfc55dc264a992e7b20dd7388ce9113b
poor314 <55570384+poor314@users.noreply.github.com>
2019-11-12 12:53:07 +0800

    Update heap_sort_06170213.py

--- HW2/heap_sort_06170213.py
+++ HW2/heap_sort_06170213.py
@@ -1,31 +1,30 @@
-def heapify(A,size,i):
-    l=2*i+1 #左子節點
-    r=2*i+2 #右子節點
-    largest=i #i=最大
-    
-    if l<size and A[l]>A[largest]:
-        largest=l 
-        
-    if r<size and A[r]>A[largest]:
-        largest=r
-        
-    if largest!=i:
-        A[i],A[largest]=A[largest],A[i] #交換
-        
-        heapify(A,size,largest)
-        
-        
-def heapsort(A):
-    size=len(A)
-    
-    for i in range(size,-1,-1):
-        heapify(A,size,i)
-    
-    for i in range(size-1,0,-1):
-        A[i],A[0]=A[0],A[i] #交換
-        
-        heapify(A,i,0)
-        
-A=[12,1,13,5,26,7,33]
-heapsort(A)
-print(A)
+class Solution(object):
+    def heapify(self,nums,size,i):
+        l=2*i+1 #左子節點
+        r=2*i+2 #右子節點
+        largest=i #i=最大
+
+        if l<size and nums[l]>nums[largest]:
+            largest=l
+
+        if r<size and nums[r]>nums[largest]:
+            largest=r
+
+        if largest!=i:
+            nums[i],nums[largest]=nums[largest],nums[i] #交換
+
+            self.heapify(nums,size,largest)
+    def heap_sort(self,nums):
+        size=len(nums)
+
+        for i in range(size,-1,-1):
+            self.heapify(nums,size,i)
+
+        for i in range(size-1,0,-1):
+            nums[i],nums[0]=nums[0],nums[i] #交換
+
+            self.heapify(nums,i,0)
+
+nums=[12,1,13,5,26,7,33]
+Solution().heap_sort(nums)
+print(nums)
